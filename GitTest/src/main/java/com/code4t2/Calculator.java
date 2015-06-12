87b89e8a86fe7e197e898098262a821254c33de6
code4t2 <mail4t2@gmail.com>
2015-06-12 16:23:06 +0530

    Discovering TDD underway

--- GitTest/src/main/java/com/code4t2/Calculator.java
+++ GitTest/src/main/java/com/code4t2/Calculator.java
@@ -1,5 +1,24 @@
 package com.code4t2;
 
 public class Calculator {
+	
+	public static final int add(final String numberString) {
+		
+		int returnValue = 0;
+		String[] numbersArray = numberString.split(",");
+		
+//		if(numbersArray.length > 2) {
+//			throw new RuntimeException("Up to two numbers separated by commas (,) are allowed");
+//		} else {
+			for(String number: numbersArray) {
+				//Integer.parseInt(number); //If NaN, exception will be thrown
+				if (!number.trim().isEmpty()) {
+					returnValue += Integer.parseInt(number);
+				}
+			}
+			return returnValue;
+//		}
+		
+	}
 
 }
--- GitTest/src/test/java/com/code4t2/TestCalculator.java
+++ GitTest/src/test/java/com/code4t2/TestCalculator.java
@@ -1,7 +1,47 @@
 package com.code4t2;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.Assert;
+
+import com.code4t2.Calculator;
 
 public final class TestCalculator {
+	
+	@Test(expected = RuntimeException.class)
+	@Ignore
+	public final void whenMoreThanTwoNumbersAreUsedThenExceptionIsThrown() {
+		Calculator.add("1,2,3");
+	}
+	
+	@Test
+	public final void whenTwoNumbersAreUsedThenNoExceptionIsThrown() {
+		Calculator.add("1,2");
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public final void whenNonNumberIsusedThenExceptionIsThrown() {
+		Calculator.add("1,X");
+	}
+	
+	@Test
+	public final void whenEmptyStringIsUsedThenReturnValueIsZero() {
+		Assert.assertEquals(0, Calculator.add(""));
+	}
+	
+	@Test 
+	public final void whenOneNumberIsUsedThenReturnTheValueOfTheSameNumber() {
+		Assert.assertEquals(3, Calculator.add("3"));
+	}
+	
+	@Test
+	public final void whenTwoNumbersAreUsedthenReturnTheValueOftheirSum() {
+		Assert.assertEquals(3+6, Calculator.add("3,6"));
+	}
+	
+	@Test
+	public final void whenAnyNumberOfNumbersAreUsedThenTheSumIsReturned() {
+		Assert.assertEquals(1+2+3+4+5, Calculator.add("1,2,3,4,5"));
+	}
 
 }
